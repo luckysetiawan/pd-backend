12956beab4c95332228cfa1d060492e005170751
Melvin ST <67085652+melvin16s@users.noreply.github.com>
2021-11-24 15:47:30 +0700

    Update Login, Logout

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -4,8 +4,10 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
 	model "pd-backend/model"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
 )
 
 // Get All Users
@@ -138,6 +140,49 @@ func DeleteUser(c *gin.Context) {
 		response.Message = "Delete User Failed Error"
 		sendUserErrorResponse(c, response)
 	}
+}
+
+// Login User...
+func Login(c *gin.Context) {
+	db := connect()
+	defer db.Close()
+
+	email := c.PostForm("email")
+
+	query := "SELECT * FROM users where email='" + email + "'"
+
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var user model.User
+	for rows.Next() {
+		if err := rows.Scan(&user.ID, &user.Nama, &user.Email, &user.NoTelp); err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+
+	var response model.UserResponse
+
+	if user.Email == email {
+		session := sessions.Default(c)
+		session.Set("email", user.Email)
+		session.Set("nama", user.Nama)
+		session.Save()
+		response.Message = "Login Success"
+		sendUserSuccessresponse(c, response)
+	} else {
+		response.Message = "Login Error"
+		sendUserErrorResponse(c, response)
+	}
+}
+
+// Logout...
+func Logout(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Clear()
+	session.Save()
 }
 
 func sendUserSuccessresponse(c *gin.Context, ur model.UserResponse) {
