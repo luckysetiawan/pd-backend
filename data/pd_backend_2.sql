935d00e069c3e440d387ad27b4d2ea1066915f17
Melvin ST <67085652+melvin16s@users.noreply.github.com>
2021-11-25 14:50:29 +0700

    Update DB
    Merged Satya's DB with master DB

--- data/pd_backend_2.sql
+++ data/pd_backend_2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2021 at 08:26 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- Generation Time: Nov 25, 2021 at 08:49 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -57,6 +57,71 @@ INSERT INTO `menus` (`id`, `nama`, `deskripsi`, `harga`, `gambar`, `varian`) VAL
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `id` int(50) NOT NULL,
+  `idCustomer` int(50) NOT NULL,
+  `idOrderDetail` int(50) NOT NULL,
+  `invoice` varchar(255) NOT NULL,
+  `waktu` datetime NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `status` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`id`, `idCustomer`, `idOrderDetail`, `invoice`, `waktu`, `alamat`, `status`) VALUES
+(0, 1, 1, 'P-2021-11-25 14:35:42', '2021-11-25 14:35:42', 'Pluto', 0),
+(40, 2, 2, 'P-2021-11-24 19:03:15', '2021-11-24 19:03:15', 'MARSSSSSSS', 2),
+(41, 2, 2, 'P-2021-11-24 20:39:19', '2021-11-24 20:39:19', 'Moon', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderdetail`
+--
+
+CREATE TABLE `orderdetail` (
+  `id` int(50) NOT NULL,
+  `pizza` int(50) NOT NULL,
+  `rating` int(50) NOT NULL,
+  `quantity` int(50) NOT NULL,
+  `totalPesanan` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orderdetail`
+--
+
+INSERT INTO `orderdetail` (`id`, `pizza`, `rating`, `quantity`, `totalPesanan`) VALUES
+(1, 1, 5, 2, 100000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `invoice` varchar(255) NOT NULL,
+  `statusPembayaran` int(10) NOT NULL,
+  `totalHarga` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`invoice`, `statusPembayaran`, `totalHarga`) VALUES
+('pay01', 1, 100000);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -64,6 +129,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `notelp` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -71,44 +137,38 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `nama`, `email`, `notelp`) VALUES
-(1, 'Dave', 'dave@gmail.com', '089123123123'),
-(2, 'Christian', 'christian@gmail.com', '089098098098'),
-(3, 'Lucky', 'lucky@gmail.com', '089234234234'),
-(4, 'Gilbert', 'gilbert@gmail.com', '089987987987'),
-(5, 'MelvinSebastian', 'melvin@gmail.com', '012345678901');
+INSERT INTO `users` (`id`, `nama`, `email`, `password`, `notelp`) VALUES
+(1, 'Dave', 'dave@gmail.com', 'dave123', '089123123123'),
+(2, 'Christian', 'christian@gmail.com', 'christian123', '089098098098'),
+(3, 'Lucky', 'lucky@gmail.com', 'lucky123', '089234234234'),
+(4, 'Gilbert', 'gilbert@gmail.com', 'gilbert123', '089987987987'),
+(5, 'MelvinSebastian', 'melvin@gmail.com', 'melvin123', '087785705296');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `menus`
+-- Indexes for table `order`
 --
-ALTER TABLE `menus`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idOrderDetail` (`idOrderDetail`),
+  ADD KEY `invoice` (`invoice`),
+  ADD KEY `idCustomer` (`idCustomer`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `orderdetail`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `orderdetail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pizza` (`pizza`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `payment`
 --
-
---
--- AUTO_INCREMENT for table `menus`
---
-ALTER TABLE `menus`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`invoice`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
